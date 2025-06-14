0d5a13017de4d74b5b825818c700221a583256be
Daniyal Khawar <daniyalkhawar3@gmail.com>
2025-06-15 00:36:43 +0500

    Initial commit

---
+++ Portfolio-main/src/utils/SkillStack.ts
@@ -0,0 +1,116 @@
+import {
+    HtmlIcon,
+    CssIcon,
+    JsIcon,
+    ReactIcon,
+    NextIcon,
+    AstroIcon,
+    TailwindIcon,
+    ReactQueryIcon,
+    ReduxIcon,
+    ZustandIcon,
+    D3JsIcon,
+    NanostoresIcon,
+    NodeIcon,
+    ExpressIcon,
+    MongoIcon,
+    FirebaseIcon,
+    EdgeStoreIcon,
+    MysqlIcon,
+    PhpIcon,
+    RestApiIcon,
+    GdriveIcon,
+    NextAuthIcon,
+    PythonIcon,
+    CppIcon,
+    JavaIcon,
+    ViteIcon,
+    SocketIoIcon,
+    NpmIcon,
+    GitIcon,
+    NetlifyIcon,
+    VercelIcon,
+    VsCodeIcon,
+    FigmaIcon,
+    PreactIcon,
+    GSAPIcon,
+    FramerMotionIcon,
+    BunIcon,
+    RIcon
+} from "../assets/Icons";
+import { GithubIcon } from "../assets/SVGs";
+
+export const FrontendStack = [
+    {
+        name: "HTML",
+        icon: HtmlIcon,
+        link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+    },
+    {
+        name: "CSS",
+        icon: CssIcon,
+        link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+    },
+    {
+        name: "JavaScript",
+        icon: JsIcon,
+        link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    },
+    { name: "React.js", icon: ReactIcon, link: "https://react.dev/" },
+    { name: "Next.js", icon: NextIcon, link: "https://nextjs.org/" },
+    { name: "Astro", icon: AstroIcon, link: "https://astro.build/" },
+    { name: "Tailwind", icon: TailwindIcon, link: "https://tailwindcss.com/" },
+    {
+        name: "TS Query",
+        icon: ReactQueryIcon,
+        link: "https://tanstack.com/query/latest/",
+    },
+    { name: "GSAP", icon: GSAPIcon, link: "https://gsap.com/" },
+    { name: "Motion", icon: FramerMotionIcon, link: "https://www.framer.com/motion/" },
+];
+export const BackendStack = [
+    { name: "Node.js", icon: NodeIcon, link: "https://nodejs.org/" },
+    { name: "Express.js", icon: ExpressIcon, link: "https://expressjs.com/" },
+    { name: "MongoDB", icon: MongoIcon, link: "https://www.mongodb.com/" },
+    {
+        name: "Firebase",
+        icon: FirebaseIcon,
+        link: "https://firebase.google.com/",
+    },
+    { name: "MySQL", icon: MysqlIcon, link: "https://www.mysql.com/" },
+    {
+        name: "REST API",
+        icon: RestApiIcon,
+        link: "https://developer.mozilla.org/en-US/docs/Glossary/REST",
+    },
+    {
+        name: "Auth.js",
+        icon: NextAuthIcon,
+        link: "https://authjs.dev/",
+    },
+];
+
+export const ProgrammingStack = [
+    {
+        name: "JavaScript",
+        icon: JsIcon,
+        link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    },
+    { name: "Python", icon: PythonIcon, link: "https://www.python.org/" },
+    { name: "C++", icon: CppIcon, link: "https://isocpp.org/" },
+    { name: "Java", icon: JavaIcon, link: "https://www.oracle.com/java/" },
+];
+
+export const ToolsStack = [
+    { name: "NPM", icon: NpmIcon, link: "https://www.npmjs.com/" },
+    { name: "Git", icon: GitIcon, link: "https://git-scm.com/" },
+    { name: "GitHub", icon: GithubIcon, link: "https://github.com/" },
+    { name: "Vite.js", icon: ViteIcon, link: "https://vitejs.dev/" },
+    { name: "Vercel", icon: VercelIcon, link: "https://vercel.com/" },
+    {
+        name: "VS Code",
+        icon: VsCodeIcon,
+        link: "https://code.visualstudio.com/",
+    },
+    { name: "Figma", icon: FigmaIcon, link: "https://www.figma.com/" },
+];
